0efc7748990b9e4046873c0b4274e556acac85fc
Mohan Raj <mohanraj252003@gmail.com>
2024-06-29 11:37:32 +0530

    origin change

--- src/main/java/com/ecommerce/full_stack/Controller/CartController.java
+++ src/main/java/com/ecommerce/full_stack/Controller/CartController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/cart")
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 public class CartController {
 
     @Autowired
